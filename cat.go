473b5b0aba6fe03b547916cf6ab927f029d337e3
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-01-26 22:44:46 +0800

    refactor: make easier to test

--- cat.go
+++ cat.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	catCommand := NewCatCommand()
+	flags, args := getFlagsAndArgs()
+	catCommand := NewCatCommand(os.Stdout, os.Stderr, flags, args)
 	catCommand.Run()
 }
 
@@ -19,12 +22,7 @@ type Flags struct {
 	nonblank bool
 }
 
-type CatCommand struct {
-	args  []string
-	flags Flags
-}
-
-func NewCatCommand() CatCommand {
+func getFlagsAndArgs() (Flags, []string) {
 	const (
 		defaultNumber   = false
 		numberUsage     = "Number lines"
@@ -41,63 +39,92 @@ func NewCatCommand() CatCommand {
 	flag.BoolVar(&nonblank, "b", false, nonblankUsage+" (shorthand)")
 
 	flag.Parse()
+
 	flags := Flags{
 		number:   number,
 		nonblank: nonblank,
 	}
 
+	return flags, flag.Args()
+}
+
+type CatCommand struct {
+	resultWriter io.Writer
+	errWriter    io.Writer
+	args         []string
+	flags        Flags
+}
+
+func NewCatCommand(resultWriter io.Writer, errWriter io.Writer, flags Flags, args []string) CatCommand {
 	return CatCommand{
-		flags: flags,
-		args:  flag.Args(),
+		flags:        flags,
+		args:         args,
+		resultWriter: resultWriter,
+		errWriter:    errWriter,
 	}
 }
 
 func (c *CatCommand) Run() {
+	var result string
+
 	if len(c.args) < 1 {
-		c.readStdin()
+		result = c.readStdin()
 	} else {
 		for _, v := range c.args {
-			c.readFile(v)
+			r, err := c.readFile(v)
+			if err != nil {
+				fmt.Fprintln(c.errWriter, "Invalid File")
+				os.Exit(1)
+			}
+
+			result += r
 		}
 	}
+
+	fmt.Fprint(c.resultWriter, result)
 }
 
-func (c *CatCommand) readFile(path string) {
+func (c *CatCommand) readFile(path string) (string, error) {
 	file, err := os.Open(path)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Invalid file")
-		os.Exit(int(flag.ExitOnError))
+		return "", errors.New("invalid file")
 	}
 
 	defer file.Close()
-	c.print(file)
+	return c.getContents(file), nil
 }
 
-func (c *CatCommand) readStdin() {
+func (c *CatCommand) readStdin() string {
 	file := os.Stdin
 	defer file.Close()
-	c.print(file)
+	return c.getContents(file)
 }
 
-func (c *CatCommand) print(file *os.File) {
+func (c *CatCommand) getContents(file *os.File) string {
+	var result string
 	scanner := bufio.NewScanner(file)
 	line := 0
 
 	for scanner.Scan() {
 		text := scanner.Text()
+		showNonblank := c.flags.nonblank && text != ""
 
 		if c.flags.number {
 			line++
-			printLineNumber(line)
-		} else if c.flags.nonblank && text != "" {
+		} else if showNonblank {
 			line++
-			printLineNumber(line)
 		}
 
-		fmt.Fprintln(os.Stdout, text)
+		result += formatText(text, line, c.flags.number || showNonblank)
 	}
+
+	return result
 }
 
-func printLineNumber(line int) {
-	fmt.Fprintf(os.Stdout, "%s%d\t", strings.Repeat(" ", 6-len(strconv.Itoa(line))), line)
+func formatText(text string, line int, showLineNumber bool) string {
+	if !showLineNumber {
+		return fmt.Sprintf("%s\n", text)
+	}
+
+	return fmt.Sprintf("%s%d\t%s\n", strings.Repeat(" ", 6-len(strconv.Itoa(line))), line, text)
 }
